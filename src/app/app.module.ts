bb86127465dfb9cddc9f45841d90d7170c53a28d
Raphael Scaziti <rscaziti@gmail.com>
2021-05-29 11:06:19 -0300

    feat(nova-transferencia/extrato): adicionado componentes, incluido ts e scss iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { AppComponent } from './app.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt-BR'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
